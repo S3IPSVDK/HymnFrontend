2389e2946360347b4d1f0eefd7d6241eade85b5b
McFinnigan <v.kouwenberg@student.fontys.nl>
2024-04-17 22:07:57 +0200

    Trying to make tailwind work

--- hymn-frontend/src/App.js
+++ hymn-frontend/src/App.js
@@ -1,16 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="text-SCGreen font-roboto">
           Learn React
         </a>
       </header>
