d45da6b7f1a56fd3cfa420d1100dc6d6144e3350
McFinnigan <v.kouwenberg@student.fontys.nl>
2024-06-19 19:51:35 +0200

    Test 4

--- src/api/SongApi.js
+++ src/api/SongApi.js
@@ -22,3 +22,4 @@ export const searchSongsByName = async (searchString) => {
     const response = await axios.get(`${API_URL}/Song/search`, { params: { searchString } });
     return response.data;
 };
+//another test, number 4
